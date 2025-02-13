43d3214f8ecff276de01b1fa77eba24257acd1ec
Nathan Silva <nathansilva.dev@gmail.com>
2025-02-13 13:16:40 -0300

    adicionando toast e configurando botao de logout

--- frontend/src/app/api/logout/route.js
+++ frontend/src/app/api/logout/route.js
@@ -1,7 +1,9 @@
 import { NextResponse } from "next/server";
 
 export async function POST(req) {
-  const response = NextResponse.redirect(new URL("/login", req.nextUrl.origin));
+  const response = NextResponse.json({
+    message: "Logout realizado com sucesso",
+  });
   response.cookies.set("token", "", { expires: new Date(0), path: "/" });
   return response;
 }
--- frontend/src/components/LogoutButton.js
+++ frontend/src/components/LogoutButton.js
@@ -1,10 +1,36 @@
 "use client";
 
+import { Toaster, toaster } from "@/components/ui/toaster";
+
 export default function LogoutButton() {
   const handleLogout = async () => {
-    await fetch("/api/logout", { method: "POST" });
-    window.location.href = "/login"; // Redirecionamento manual
+    try {
+      const res = await fetch("/api/logout", { method: "POST" });
+
+      if (res.ok) {
+        toaster.create({
+          title: "Logout realizado com sucesso!",
+          type: "success",
+        });
+        setTimeout(() => {
+          window.location.href = "/login";
+        }, 1000);
+      } else {
+        throw new Error("Erro ao fazer logout");
+      }
+    } catch (error) {
+      console.error(error);
+      toaster.create({
+        title: "Erro ao fazer logout",
+        type: "error",
+      });
+    }
   };
 
-  return <button onClick={handleLogout}>Sair</button>;
+  return (
+    <>
+      <button onClick={handleLogout}>Sair</button>
+      <Toaster />
+    </>
+  );
 }
