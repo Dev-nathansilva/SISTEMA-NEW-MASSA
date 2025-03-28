7c84cc1876d1dd1622fd9d702b713384ac697774
Nathan Silva <nathansilva.dev@gmail.com>
2025-03-27 23:41:13 -0300

    Update layout.js

--- frontend/src/app/layout.js
+++ frontend/src/app/layout.js
@@ -2,13 +2,20 @@ import "./globals.css";
 import { Suspense } from "react";
 import { Provider } from "@/components/ui/provider";
 import AuthProvider from "@/context/AuthContext";
+import Spinner from "@/components/Spinner";
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en" suppressHydrationWarning>
       <body>
         <Provider>
-          <Suspense fallback={<div>Carregando...</div>}>
+          <Suspense
+            fallback={
+              <div className="flex justify-center">
+                <Spinner />
+              </div>
+            }
+          >
             <AuthProvider>{children}</AuthProvider>
           </Suspense>
         </Provider>
